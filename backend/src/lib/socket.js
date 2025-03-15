c605ee546d156c7b900aaf570582cd4332014cd9
tyhow99 <122315624+tyhow99@users.noreply.github.com>
2025-03-15 17:37:31 -0400

    Please upload

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5001"]
+        origin: ["http://localhost:5002"]
     }
 });
 
